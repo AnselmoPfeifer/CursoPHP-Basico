123f7a8d88da335301fbab04089c6ca1c6b7face
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-03-29 19:38:52 -0400

    Principais Operadores, Incremento e Concatenar

--- Aula-01/04-Principais-Operadores.php
+++ Aula-01/04-Principais-Operadores.php
@@ -5,6 +5,8 @@
 	$subtrai = $x-$y;
 	$multiplicacao = $x*$y;
 	$divide = $x/$y;
+	$primeiroNome = " Joao";
+	$segundoNome = " da Silva"
 ?>
 <!Doctype html>
 <html>
@@ -17,6 +19,9 @@
 	  	<h1>Resultado das operaçoes</h1>
 	  <hr>
 	  <p>
+	  	<b>Os valores informados foram <?php echo 'X='.$x.' e Y='.$y; ?></b>
+	  </p>
+	  <p>
 	  		A soma é: <?php echo $soma;?>
 	  </p>
 	  <p>
@@ -25,9 +30,26 @@
 	  <p>
 	  		A Multiplicação é: <?php echo $multiplicacao;?>
 	  </p>
-	 	<p>
+	  <p>
 	  		A Divisão é: <?php echo $divide;?>
 	  </p>
-	  
+	  <p>
+	  	Incrementando o valor de da Soma: <?php echo $soma++; ?>
+	  </p>
+	  <p>
+	  	Adicionado 5 no valor de da Soma: <?php echo $soma+= 5; ?>
+	  </p>
+	  <p>
+	  	Removendo  5 no valor de da Soma: <?php echo $soma-= 5; ?>
+	  </p>
+	  <hr>
+	  	<h2>Concatenar variaveis</h2>
+	  	<p>
+	  		Seu nome completo é : <b><?php echo $primeiroNome.	$segundoNome; ?></b>.
+	  	</p>
+	   	<p>
+	  		Completrando um valor na variavel : <b><?php echo $primeiroNome.= " da Silva"; ?></b>.
+	  	</p>
+	  <hr>
 	</body>
 </html>
