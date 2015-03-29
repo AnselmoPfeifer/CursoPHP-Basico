1b87e05edab9403c15d51d7633bb9fe9cc9d4546
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-03-29 16:11:53 -0400

    Trabalhando com variaveis

---
+++ Aula-01/02-Trabalhando-com-Variaveis.php
@@ -0,0 +1,11 @@
+<?php
+	//Para valores em numeros nao vai ' ' nem " "
+	$x = 1;
+	//Para valores em string tem que usar ' ' ou " "
+	$nome = "Seu Nome";
+	//Imprimindo a variavel x
+	echo $x;
+	echo "\n";
+	//Imprimindo a variavel nome;
+	echo $nome;
+?>
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CursoPHP-Basico
+Aula 01 -
+	Iniciando com PHP
+		Ola Mundo
+		Trabalhando com Variaveis
+		Imprimindo valores na tela
+		Principais Operadores
+		Quanto é dois mais dois dividido por dois
+		Pense em um numero
+		Site a jato com PHP
+	
