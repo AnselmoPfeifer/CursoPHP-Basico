39cff676edf9998be7db48e5dfe92cf6b27d0362
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-03-29 16:25:05 -0400

    Imprimindo Valores na Tela

---
+++ Aula-01/03-Imprimindo-Valores-Tela.php
@@ -0,0 +1,12 @@
+<?php
+	$nome = "Joao da Silva";
+?>
+<html>
+	<head>
+	  <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+	  <title>03-Imprimindo-Valores-Tela</title>
+	</head>
+	<body>
+	  <h2>Seu nome é: <?php echo $nome; ?></h2>
+	</body>
+</html>
