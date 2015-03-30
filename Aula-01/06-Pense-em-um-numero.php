50a53fd0d6df68789756a426aeb42366ac7ce7db
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-03-29 20:57:33 -0400

    Pense em um numero

--- Aula-01/06-Pense-em-um-numero.php
+++ Aula-01/06-Pense-em-um-numero.php
@@ -11,12 +11,12 @@
 	<body>
 	  <hr>
 	  <h1>O resultado sempre será 5</h1>
-	  <h2>Pense num numero, Multiplique por 2; agora some com 10; Agora divida por 2.<br>
+	  <h2>Pense num numero,(O valor foi <?php echo $x; ?>) Multiplique por 2; agora some com 10; Agora divida por 2.<br>
 	  	Com o valor faça (-) menos o numero que vc <b>pensou</b>.
 	  </h2>
 	  <hr>
 	  	<h2> O Resultado eh:
 	  		<?php echo $resultado ?>
-	  		</h2>
+	  	</h2>
 	</body>
 </html>
