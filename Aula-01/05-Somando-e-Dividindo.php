3784c139ec7633e52e18663a4363fc22d4049d94
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-03-29 20:43:46 -0400

    Trabalhando com Operacoes juntas

---
+++ Aula-01/05-Somando-e-Dividindo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	   <meta charset="utf-8">
+	  <title>03-Imprimindo-Valores-Tela</title>
+	</head>
+	<body>
+	  <hr>
+	  <h2>Quanto é 2+2/2/4+10?
+	  </h2>
+	  <hr>
+	  	<h2> A resposta é: <?php echo ((((2+2)/2)/4)+10); ?></h2>
+	</body>
+</html>
