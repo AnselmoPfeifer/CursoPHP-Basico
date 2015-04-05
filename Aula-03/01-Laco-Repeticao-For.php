7e17b8f4ceaeb7b43eaf77db9eec4be0c298470b
Anselmo Pfeifer <anspfeifer@gmail.com>
2015-04-05 18:53:52 -0400

    laco de repeticao FOR

--- Aula-03/01-Laco-Repeticao-For.php
+++ Aula-03/01-Laco-Repeticao-For.php
@@ -1,3 +1,14 @@
-<?php
-	
-?>
+<!-- Usando o for normal -->
+<ol>
+	<?php
+		for($i=0;$i<10;$i++)
+			echo '<li><a href="#'.$i.'">Home</a></li>';
+	?>
+</ol>
+
+<!-- Versao contraida do for -->
+<ol>
+<?php for($i=0;$i<10;$i++):?>
+		<li><a href="#<?=$i; ?>">Home</</a></li>	
+<?php endfor; ?>
+</ol>
